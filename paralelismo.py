bc0c2f2d16e6ab6cae458908d38d335d9f6190e3
Samara Vitória <devsamaravitoria@gmail.com>
2024-08-06 08:58:59 -0300

    feat: Novo arquivo sobre paralelismo

---
+++ paralelismo.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from multiprocessing import Pool
+
+base = pd.read_csv("C:/Users/samar/Downloads/archive (5)/Student_performance_data _.csv",
+                   encoding="ISO-8859-1")
